222bb37f5383d23f1b233e3b89a75acbecc16a36
Miguel Campos <amodestduck@gmail.com>
2023-11-13 17:09:54 -0800

    test storybook

---
+++ src/components/navigation/navigation-bar.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta } from '@storybook/react';
+import { NavigationBar } from './navigation-bar';
+
+const Story: Meta<typeof NavigationBar> = {
+  component: NavigationBar,
+  title: 'Navigation/NavigationBar',
+};
+export default Story;
+
+export const Primary = {
+  args: {
+   
+  },
+};
---
+++ src/components/navigation/navigation-bar.tsx
@@ -0,0 +1,5 @@
+import { Box } from "@mui/material"
+
+export const NavigationBar = () => {
+    return <Box>Hello World</Box>
+}   
