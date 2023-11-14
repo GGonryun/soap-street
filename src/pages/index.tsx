22ce3d0752b0ef8020c5a0e2ccddcbed909b0c1f
Miguel Campos <amodestduck@gmail.com>
2023-11-13 19:03:37 -0800

    update home page with layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,28 @@
-import Head from "next/head";
-import { Box } from "@mui/material";
+import { Box, Divider, Link, Typography, styled } from "@mui/material";
+import { Layout } from "@/components/layout/layout";
+import { getCurrentYear } from "@/util/time";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Soap Street</title>
-        <meta
-          name="description"
-          content="Bubbles, Barter, Bliss - Where soap dreams come true, and cleanliness is currency"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <Box>Home Page</Box>
-      </main>
+      <Layout
+        title="Soap Street"
+        description="Bubbles, Barter, Bliss - Where soap dreams come true, and cleanliness is currency"
+      >
+        <Typography variant="h5">Products</Typography>
+        <Divider />
+        <Box display="flex" flexGrow={1} justifyContent="space-between">
+          <CaptionText>
+            <Link href="/register">Sign Up</Link>
+          </CaptionText>
+          <CaptionText>© {getCurrentYear()} Soap Street</CaptionText>
+        </Box>
+      </Layout>
     </>
   );
 }
+
+const CaptionText = styled(Typography)(({ theme }) => ({
+  color: theme.palette.text.secondary,
+  fontSize: "0.7rem",
+}));
