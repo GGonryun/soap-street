e2126fba21e7d55a0d654ffd2eb3f7036c9dce11
Miguel Campos <amodestduck@gmail.com>
2023-11-14 23:21:02 -0800

    support vercel blob urls in next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["9u5sq01tduqtq9hy.public.blob.vercel-storage.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
