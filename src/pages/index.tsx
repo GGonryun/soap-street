9c076eeadf55854744aa8e36b0af4bbb9b97ebbe
Miguel Campos <amodestduck@gmail.com>
2023-11-14 23:23:34 -0800

    show product lissts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,47 @@
-import { Divider, Typography } from "@mui/material";
+import { Box, Button, Divider, Link, Typography } from "@mui/material";
 import { Layout } from "@/components/layout/layout";
 import { RegistrationFooter } from "@/components/footer/registration-footer";
+import { ProductCard } from "@/components/cards/product-card";
+import { useShoppingCart } from "@/hooks/useShoppingCart";
+import { trpc } from "@/trpc/client";
+import { ProductObject } from "@/types/create-product-form";
+import { MarketProductCard } from "@/components/cards/market-product-card";
 
 export default function Home() {
+  const cart = useShoppingCart();
+  const handleAction = (id: string) => {
+    // add to cart
+    cart.setItems((items) => [...items, id]);
+  };
+
+  const filterProductQuantity = (product: ProductObject): number => {
+    const quantity = cart.items.filter((item) => item === product.id).length;
+
+    return Math.max(product.quantity - quantity, 0);
+  };
+
+  const { data: products } = trpc.products.list.useQuery();
+
   return (
     <Layout
       title="Soap Street"
       description="Bubbles, Barter, Bliss - Where soap dreams come true, and cleanliness is currency"
     >
-      <Typography variant="h5">TODO: Products</Typography>
-      <Divider />
+      <Typography variant="body2" pt={2}>
+        Bubbles, Barter, Bliss - Where soap dreams come true, and cleanliness is
+        currency
+      </Typography>
+      <Divider sx={{ my: 1 }} />
+      <Box display="flex" gap={1} flexWrap={"wrap"} py={1}>
+        {products?.map((product) => (
+          <Box key={product.id} flex={1} maxWidth={400} minWidth={200}>
+            <MarketProductCard
+              product={product}
+              onClick={() => handleAction(product.id)}
+            />
+          </Box>
+        ))}
+      </Box>
       <RegistrationFooter />
     </Layout>
   );
--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -3,9 +3,27 @@ import { Layout } from "@/components/layout/layout";
 import { Add } from "@mui/icons-material";
 import { useState } from "react";
 import { CreateProductModalContainer } from "@/components/modals/create-product-modal-container";
+import { trpc } from "@/trpc/client";
+import { ProductCard } from "@/components/cards/product-card";
+import { SubmissionButton } from "@/components/buttons/submission-button";
 
 export default function Products() {
+  const [editId, setEditId] = useState<string | undefined>(undefined);
   const [showModal, setShowModal] = useState(false);
+  const { data: products } = trpc.products.list.useQuery();
+
+  const handleClose = () => {
+    setShowModal(false);
+    setEditId(undefined);
+  };
+
+  const handleEdit = (id: string) => {
+    // set the edit id in the modal
+    // open the modal
+    setEditId(id);
+    setShowModal(true);
+  };
+
   return (
     <>
       <Layout title="Products" description="Make an account!">
@@ -21,11 +39,26 @@ export default function Products() {
               Create New
             </Button>
           </Box>
+          <Box display="flex" gap={1} flexWrap={"wrap"} pt={1}>
+            {products?.map((product) => (
+              <Box key={product.id} flex={1} maxWidth={400} minWidth={200}>
+                <ProductCard
+                  {...product}
+                  onClick={() => handleEdit(product.id)}
+                >
+                  <SubmissionButton onClick={() => handleEdit(product.id)}>
+                    Edit
+                  </SubmissionButton>
+                </ProductCard>
+              </Box>
+            ))}
+          </Box>
         </Container>
       </Layout>
       <CreateProductModalContainer
+        id={editId}
         open={showModal}
-        onClose={() => setShowModal(false)}
+        onClose={handleClose}
       />
     </>
   );
